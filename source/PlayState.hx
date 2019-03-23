d6a00f2c0fc2c253a64cceac169538d7b52b70bb
eirikwitt <eirikw@live.no>
2019-03-23 23:56:06 +0100

    Fixed higlight allignment

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -154,7 +154,7 @@ class PlayState extends FlxState {
 			obstacles.sort(FlxSort.byY);
 		}
 
-		rightPlayerHighlightBox.x = Math.floor((rightPlayer.x)/tileWidth - 0.5) * tileWidth + 5;
+		rightPlayerHighlightBox.x = Math.floor((rightPlayer.x)/tileWidth - 0.5) * 64;
 		rightPlayerHighlightBox.y = Math.round(rightPlayer.y/tileHeight) * 64;
 		
 		if (rightPlayer.building && rightPlayerHighlightBox.x > midline.x) {
