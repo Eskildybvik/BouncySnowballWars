d33aeef1f67a593d204593ce6835a4832d08da07
Eskil Dybvik <eskildybvik234@gmail.com>
2019-03-23 11:10:07 +0100

    Added temporary walls

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.group.FlxSpriteGroup;
 import flixel.input.gamepad.id.LogitechID;
 import flixel.util.FlxColor;
 import flixel.FlxG;
@@ -8,6 +9,8 @@ import flixel.FlxState;
 
 class PlayState extends FlxState {
 	private var midline:FlxSprite;
+	private var tempWalls:FlxSpriteGroup;
+	private var leftPlayer:Player;
 
 	override public function create():Void {
 		super.create();
@@ -17,11 +20,30 @@ class PlayState extends FlxState {
 		midline.screenCenter(X);
 		add(midline);
 
-		add(new Player(128, 128)); // temporary
+		leftPlayer = new Player(128, 128);
+		add(leftPlayer);
 		add(new SnowBall(156, 228));
+
+		// temp walls
+		tempWalls = new FlxSpriteGroup(0, 0);
+		var topWall = new FlxSprite(0, 0);
+		topWall.makeGraphic(FlxG.width, 32, FlxColor.GREEN);
+		tempWalls.add(topWall);
+		var leftWall = new FlxSprite(0, 32);
+		leftWall.makeGraphic(32, FlxG.height - 64, FlxColor.GREEN);
+		tempWalls.add(leftWall);
+		var bottomWall = new FlxSprite(0, FlxG.height-32);
+		bottomWall.makeGraphic(FlxG.width, 32, FlxColor.GREEN);
+		tempWalls.add(bottomWall);
+		var rightWall = new FlxSprite(FlxG.width - 32, 32);
+		rightWall.makeGraphic(32, FlxG.height-64, FlxColor.GREEN);
+		tempWalls.add(rightWall);
+		add(tempWalls);
+		tempWalls.immovable = true;
 	}
 
 	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
+		FlxG.collide(leftPlayer, tempWalls);
 	}
 }
