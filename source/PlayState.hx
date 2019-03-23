f79c55f3c230a3e6283b08677738ce4b0aa21f4b
Eskil Dybvik <eskildybvik234@gmail.com>
2019-03-23 09:50:09 +0100

    Added midline

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,10 +1,20 @@
 package;
 
+import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
 
 class PlayState extends FlxState {
+	private var midline:FlxSprite;
+
 	override public function create():Void {
 		super.create();
+
+		midline = new FlxSprite(0, 0);
+		midline.makeGraphic(8, FlxG.height, FlxColor.RED);
+		midline.screenCenter(X);
+		add(midline);
 	}
 
 	override public function update(elapsed:Float):Void {
