a31fcc68ba328a7456b6636bd4cc05748225c2ec
SondreElg <sondrhel@stud.ntnu.no>
2019-03-23 14:37:22 +0100

    Obstacle placement

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,15 +7,30 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.tile.FlxTilemap;
+import flixel.util.FlxSpriteUtil;
 
 class PlayState extends FlxState {
-	//For obstacles and snow tiles
+	// for obstacles and snow tiles
 	static inline var tileWidth:Int = 64;
 	static inline var tileHeight:Int = 64;
-	var obstacleMap:FlxTilemap;
-	var snowMap:FlxTilemap;
-	var highlightBox:FlxSprite;
+	private var obstacleMap:FlxTilemap;
+	private var snowMap:FlxTilemap;
+	private var leftPlayerHighlightBox:FlxSprite;
+	private var obstacleMapData:Array<Array<Int>> = [
+	[0,0,0,0,1,0,0,0,0,-9,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,1,0,0,0,0,-9,0,0,0,0,0,1,0,0,0,0],
+	[0,0,0,0,1,0,0,0,0,-9,0,0,0,0,0,1,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,-9,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,1,0,-9,0,1,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,1,0,-9,0,1,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,1,0,-9,0,1,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,-9,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,1,0,0,0,0,-9,0,0,0,0,0,1,0,0,0,0],
+	[0,0,0,0,1,0,0,0,0,-9,0,0,0,0,0,1,0,0,0,0],
+	[0,0,0,0,1,0,0,0,0,-9,0,0,0,0,0,0,0,0,0,1]];	
 
+	private var wallThickness:Int = 7;
+	private var xOffset:Int = 32;
 	private var midline:FlxSprite;
 	private var tempWalls:FlxSpriteGroup;
 	private var leftPlayer:Player;
@@ -40,19 +55,31 @@ class PlayState extends FlxState {
 		// temp walls
 		tempWalls = new FlxSpriteGroup(0, 0);
 		var topWall = new FlxSprite(0, 0);
-		topWall.makeGraphic(FlxG.width, 32, FlxColor.GREEN);
+		topWall.makeGraphic(FlxG.width, wallThickness + 1, FlxColor.GREEN);
 		tempWalls.add(topWall);
-		var leftWall = new FlxSprite(0, 32);
-		leftWall.makeGraphic(32, FlxG.height - 64, FlxColor.GREEN);
+		var leftWall = new FlxSprite(0, wallThickness);
+		leftWall.makeGraphic(wallThickness, FlxG.height - (wallThickness * 2), FlxColor.GREEN);
 		tempWalls.add(leftWall);
-		var bottomWall = new FlxSprite(0, FlxG.height-32);
-		bottomWall.makeGraphic(FlxG.width, 32, FlxColor.GREEN);
+		var bottomWall = new FlxSprite(0, FlxG.height - wallThickness);
+		bottomWall.makeGraphic(FlxG.width, wallThickness, FlxColor.GREEN);
 		tempWalls.add(bottomWall);
-		var rightWall = new FlxSprite(FlxG.width - 32, 32);
-		rightWall.makeGraphic(32, FlxG.height-64, FlxColor.GREEN);
+		var rightWall = new FlxSprite(FlxG.width - wallThickness, wallThickness);
+		rightWall.makeGraphic((wallThickness * 2), FlxG.height - (wallThickness * 2), FlxColor.GREEN);
 		tempWalls.add(rightWall);
 		add(tempWalls);
 		tempWalls.immovable = true;
+
+		// tilemap stuff begins here
+		obstacleMap = new FlxTilemap();
+
+		obstacleMap.loadMapFrom2DArray(obstacleMapData, "assets/images/white.jpg", 64, 64, null, 0, 1, 1);
+		obstacleMap.setPosition(xOffset, wallThickness + 1);
+		add(obstacleMap);
+
+		leftPlayerHighlightBox = new FlxSprite(0, 0);
+		leftPlayerHighlightBox.makeGraphic(tileWidth, tileHeight, FlxColor.TRANSPARENT);
+		FlxSpriteUtil.drawRect(leftPlayerHighlightBox, 0, 0, tileWidth, tileHeight, FlxColor.TRANSPARENT, { thickness: 1, color: FlxColor.RED });
+		add(leftPlayerHighlightBox);
 	}
 
 	override public function update(elapsed:Float):Void {
@@ -62,6 +89,15 @@ class PlayState extends FlxState {
 		FlxG.collide(snowBall, tempWalls);
 		FlxG.collide(snowBall, leftPlayer); // må fikse slikk at ballen ikke dytter på spiller
 
+		FlxG.collide(leftPlayer, obstacleMap);
+		FlxG.collide(snowBall, obstacleMap); // må fikse slik at snøballer som kastes kolliderer med veggene
+
+		// obstacle placement
+		leftPlayerHighlightBox.x = Math.ceil((leftPlayer.x - xOffset)/tileWidth + 1) * tileWidth + xOffset;
+		leftPlayerHighlightBox.y = Math.round(leftPlayer.y/tileHeight) * tileHeight + wallThickness;
 		
+		if (FlxG.keys.pressed.B) {
+			obstacleMap.setTile(Math.ceil((leftPlayer.x - xOffset)/tileWidth) + 1, Math.round(leftPlayer.y/tileHeight), 1);
+		}
 	}
 }
