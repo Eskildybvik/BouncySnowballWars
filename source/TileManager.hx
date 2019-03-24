9254157d8445454c889adf6f6dbdcfc0323fcc39
eirikwitt <eirikw@live.no>
2019-03-24 16:23:29 +0100

    Added pickup animation

--- source/TileManager.hx
+++ source/TileManager.hx
@@ -141,4 +141,32 @@ class TileManager{
         }
         
     }
+
+    public function canGetSnow(player:Player):Bool{
+        var coords:FlxPoint = new FlxPoint(player.x + 32, player.y + 32);
+        if(player.isRightPlayer){
+            if( Reg.rightPlayerSnow >= 30){
+                return false;
+            }
+            var index:Int = rightDirt.getTileIndexByCoords(coords);
+            if(rightDirt.getData()[index] > 1){
+                return(true);
+            }
+            if(rightIce.getData()[index] > 1){
+                return(true);
+            }
+        }else{
+            if( Reg.leftPlayerSnow >= 30){
+                return false;
+            }
+            var index:Int = leftDirt.getTileIndexByCoords(coords);
+            if(leftDirt.getData()[index] > 1){
+                return(true);
+            }
+            if(leftIce.getData()[index] > 1){
+                return(true);
+            } 
+        }
+        return false;
+    }
 }
