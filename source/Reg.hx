f0fb83bcb05fc6e2060e32a3eae737c39c94b2a9
SondreElg <sondrhel@stud.ntnu.no>
2019-03-24 15:36:52 +0100

    Start with 5 snow

--- source/Reg.hx
+++ source/Reg.hx
@@ -1,8 +1,8 @@
 package;
 
 class Reg {
-	public static var leftPlayerSnow = 0;
-	public static var rightPlayerSnow = 0;
+	public static var leftPlayerSnow = 5;
+	public static var rightPlayerSnow = 5;
 	public static var leftPlayerHearts = 5;
 	public static var rightPlayerHearts = 5;
 }
