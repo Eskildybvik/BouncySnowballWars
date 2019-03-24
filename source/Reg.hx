7d1164bd0b8aaf6bf92b4f4852af0227878c3c83
Eskil Dybvik <eskildybvik234@gmail.com>
2019-03-24 14:40:07 +0100

    Changed initial snow to 0

--- source/Reg.hx
+++ source/Reg.hx
@@ -1,8 +1,8 @@
 package;
 
 class Reg {
-	public static var leftPlayerSnow = 30;
-	public static var rightPlayerSnow = 30;
+	public static var leftPlayerSnow = 0;
+	public static var rightPlayerSnow = 0;
 	public static var leftPlayerHearts = 5;
 	public static var rightPlayerHearts = 5;
 }
